f21264a0ec3938835643eeb26b093c0b9eb79124
TechFortune-maker <osegomere222@gmail.com>
2024-08-01 05:52:31 +0200

    Add files via upload

---
+++ GreetingsApp.java
@@ -0,0 +1,22 @@
+
+package greetingsapp;
+
+import za.ac.tut.gui.GreetingsGUI;
+
+/**
+ *
+ * @author Osego
+ */
+public class GreetingsApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        GreetingsGUI myGUI = new GreetingsGUI();
+  
+        
+    }
+    
+}
